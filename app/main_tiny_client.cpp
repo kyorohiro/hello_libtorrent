2b7fdc3d534c24b977ffda026bae3ed8ceff7a44
kyorohiro <kyorohiro@gmail.com>
2021-01-22 14:10:21 +0900

    mod tiny client

--- app/main_tiny_client.cpp
+++ app/main_tiny_client.cpp
@@ -4,25 +4,51 @@
 #include<libtorrent/session.hpp>
 #include<libtorrent/alert_types.hpp>
 #include<libtorrent/alert.hpp>
+#include<libtorrent/magnet_uri.hpp>
+#include<libtorrent/error_code.hpp>
 
 void print_usage(std::string);
 
 std::string my_listen_interfaces = "";// "0.0.0.0:6881,[::]:6881"  "{E4F0B674-0DFC-48BB-98A5-2AA730BDB6D6}:7777"
-std::string target_torrentfile_path = "./a.torrent";
+std::string target_torrentfile_path = "";
+std::string target_magnet_link = "";//magnet:?xt=urn...
 
 int main(int argc, char *argv[]) try {
-	lt::settings_pack p;
-	p.set_int(lt::settings_pack::alert_mask, lt::alert_category::all);
-    if(my_listen_interfaces.length() != 0) {
-        p.set_str(lt::settings_pack::listen_interfaces, my_listen_interfaces);
+
+    if(argc > 1) {
+        // 
+        std::string tmp = std::string(argv[1]);
+        if(tmp.rfind("magnet:",0) == 0) {
+            target_magnet_link = tmp;
+            target_torrentfile_path = "";
+        } else {
+            target_magnet_link = "";
+            target_torrentfile_path = tmp;
+        }
     }
-    lt::session session(p);
 
-	lt::add_torrent_params atp;// = lt::parse_magnet_uri("<your magnet link>");
-	atp.save_path = ".data"; // save in current dir
-    atp.ti = std::make_shared<lt::torrent_info>(target_torrentfile_path);
-	lt::torrent_handle h = session.add_torrent(std::move(atp));
+	lt::settings_pack session_params;
+	session_params.set_int(lt::settings_pack::alert_mask, lt::alert_category::all);
+    if(my_listen_interfaces.length() != 0) {
+        session_params.set_str(lt::settings_pack::listen_interfaces, my_listen_interfaces);
+    }
+    lt::session session(session_params);
 
+	lt::add_torrent_params torrent_params;
+    torrent_params.save_path = ".data"; // save in this dir
+    if(target_torrentfile_path.length() != 0) {
+    	lt::torrent_handle h = session.add_torrent(std::move(torrent_params));
+        torrent_params.ti = std::make_shared<lt::torrent_info>(target_torrentfile_path);
+    } else {
+        lt::error_code ec;
+		lt::parse_magnet_uri(target_magnet_link,torrent_params, ec);
+		if (ec){
+            std::cerr << "wrong magnet link "  << target_magnet_link << std::endl;
+            throw std::invalid_argument("wrong magnet link \""+ target_magnet_link+"\"");
+        }
+        torrent_params = lt::parse_magnet_uri(target_magnet_link);
+    }
+    session.add_torrent(torrent_params);
 
     while (true)
     {
